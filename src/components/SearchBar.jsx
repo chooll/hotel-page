2a1eca3ca154c4ff249d90a3316583c8ba41bc1f
chooll <good.anat@yandex.ru>
2022-05-04 16:35:13 +0300

    Add create request

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,24 +4,55 @@ import "react-datepicker/dist/react-datepicker.css"
 import ChildPanel from "./ChildPanel.jsx";
 
 function SearchBar () { 
- 
+
+    const [nDate, setNDate]  = useState(0);
+
+    const _formatDate = (date) => {
+        var dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+        
+        var mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+        
+        var yy = date.getFullYear();
+        
+        return dd + '.' + mm + '.' + yy;
+    }
     // Отвечает за ввод даты заезда
     const [inSelectedDate, inSetSelectedDate] = useState(new Date());
-
     // Отвечает за ввод даты выезда
     const [outSelectedDate, outSetSelectedDate] = useState(new Date());
+    const [selectHuman, setSelectHuman] = useState(1);
 
     const checkDate = (date) => {
         if (date > outSelectedDate) {
             inSetSelectedDate(date);
             outSetSelectedDate(date);
         } 
-        
         else {
             inSetSelectedDate(date)
         }
     }
-    // Модальное окно с детьми
+
+    const subDateDay = (date_1, date_2) => { return (date_1 - date_2) / (3600 * 24 * 1000) }
+
+    const sendRequest = () => {
+        let age_man = 36;
+        let dateIn = _formatDate(inSelectedDate)
+        let p  = '';
+
+        for (let i = 0; i != selectHuman; i++) {
+            p += '0' + dateIn.split('.')[0] + dateIn.split('.')[1] + (dateIn.split('.')[2] - age_man) + '0';
+            if (i != selectHuman - 1) {
+                p += '.';
+            }
+        } 
+
+        let f7 = subDateDay(outSelectedDate, inSelectedDate);
+
+        console.log(`/hotel24?xml=31&action=price&tid=211&flt=100410000050&flt2=100510000863&id_price=-1&p=${p}&data=${dateIn}&d2=1&f7=${f7}&F4=199010240062&promocode=&mode=ROOMS`)
+    };
+
     return (
     <div className="sort__panel">
         <div className="inp-date-start">
@@ -44,7 +75,7 @@ function SearchBar () {
 
         <div className="inp-amount-people">
             <p>Взрослые</p>
-            <select className="inp-date">
+            <select className="inp-date" onChange={event => setSelectHuman(event.target.value)}>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -67,7 +98,7 @@ function SearchBar () {
 
         <div className="button-sort">
             <p className="unvisible">Поиск</p>
-            <button className="search-button right-border-round">Найти номер<span className="material-symbols-outlined">search</span></button>
+            <button onClick={sendRequest} className="search-button right-border-round">Найти номер<span className="material-symbols-outlined">search</span></button>
         </div>
 
     </div>
